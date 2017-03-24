4662e6642140f80247fee010e68a1c73f1457d09
emperor-han <han.some.htx@gmail.com>
2017-03-24 16:00:16 +0800

    improved DB operaete

--- java/model/DB.java
+++ java/model/DB.java
@@ -4,21 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.PreparedStatement;
-import com.mysql.jdbc.ResultSet;
 
 public class DB {
 	
 	//声明一个数据库连接变量 和 一个结果集对象
-	private static Connection connection = null;
-	private static PreparedStatement  ps = null;
-	private static ResultSet rs = null;
+	public static Connection connection = null;
 	
-    //连接数据库并返回一个 PrepareStatement 对象
-    public static PreparedStatement connectAndGetStmt(String query){
-    	
+	//连接数据库并返回这个连接
+	public static Connection getConnection(){
     	try {
-    		//连接数据库并返回一个 PrepareStatement 对象
             Class.forName(Config.DB_DRIVER);
             //创建一个连接对象
             connection = DriverManager.getConnection(Config.DB_URL, Config.DB_USER, Config.DB_PASSWORD);
@@ -26,33 +20,14 @@ public class DB {
         } catch (Exception e) {
             System.out.println("数据库连接异常");
             e.printStackTrace();
+        }finally{
+        	return connection;
         }
-        
-        try{
-        	//创建一个 PrepareStatement 对象
-        	ps=(PreparedStatement) connection.prepareStatement(query); 
-        	return ps;
-        }catch(Exception e){
-        	e.printStackTrace();
-        }
-		return ps;
-        
-    }
-    
-    //根据 PrepareStatement 对象 返回查询一个结果集
-    public static ResultSet getQueryResult(){
-    	try {
-			return (ResultSet) ps.executeQuery();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return rs;
-    }
-  
-    
+
+	}
+	
     //关闭这个连接
-    public  static void closeConnection(){
+    public  static void closeConnection(Connection connection){
         if(connection != null){
             try {
             	connection.close(); // 关闭数据库连接
